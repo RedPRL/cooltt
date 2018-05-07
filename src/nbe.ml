f251207f0527ff0fe28b60cf9b3c5e7597dd7db9
Danny Gratzer <jozefg@cmu.edu>
2018-05-07 01:03:50 -0400

    Added sigma with natural eta rule

--- src/nbe.ml
+++ src/nbe.ml
@@ -5,6 +5,7 @@ struct
     | Var of int (* DeBruijn indices for variables *)
     | Nat | Zero | Suc of t | NRec of t * t * t * t
     | Pi of t * t | Lam of t | Ap of t * t
+    | Sig of t * t | Pair of t * t | Fst of t | Snd of t
     | Uni of uni_level
     | Subst of t * subst
   and subst =
@@ -29,10 +30,14 @@ struct
     | Zero
     | Suc of t
     | Pi of t * clos
+    | Sig of t * clos
+    | Pair of t * t
     | Uni of Syn.uni_level
   and ne =
     | Var of int (* DeBruijn levels for variables *)
     | Ap of ne * nf
+    | Fst of ne
+    | Snd of ne
     | NRec of ann_clos * nf * clos2 * ne
   and nf =
     | Normal of t * t
@@ -56,6 +61,21 @@ and do_rec env tp zero suc n =
     let zero' = D.Normal (do_ann_clos tp D.Zero, zero) in
     D.Neutral (final_tp, D.NRec (tp, zero', suc, e))
   | _ -> failwith "Not a number"
+
+and do_fst p =
+  match p with
+  | D.Pair (p1, p2) -> p1
+  | D.Neutral (D.Sig (t, clo), ne) ->
+    D.Neutral(t, D.Fst ne)
+  | _ -> failwith "Couldn't fst argument in do_fst"
+
+and do_snd p =
+  match p with
+  | D.Pair (p1, p2) -> p2
+  | D.Neutral (D.Sig (t, clo), ne) ->
+    let fst = do_fst p in
+    D.Neutral (do_clos clo fst, D.Snd ne)
+  | _ -> failwith "Couldn't snd argument in do_snd"
 
 and do_clos (t, env) a = eval t (a :: env)
 
@@ -88,18 +108,33 @@ and eval t env =
     D.Pi (eval src env, (dest, env))
   | Syn.Uni i -> D.Uni i
   | Syn.Lam t -> D.Lam (t, env)
-  | Ap (t1, t2) -> do_ap (eval t1 env) (eval t2 env)
-  | Subst (t, subst) -> eval t (apply_subst subst env)
+  | Syn.Ap (t1, t2) -> do_ap (eval t1 env) (eval t2 env)
+  | Syn.Subst (t, subst) -> eval t (apply_subst subst env)
+  | Syn.Sig (t1, t2) -> D.Sig (eval t1 env, (t2, env))
+  | Syn.Pair (t1, t2) -> D.Pair (eval t1 env, eval t2 env)
+  | Syn.Fst t -> do_fst (eval t env)
+  | Syn.Snd t -> do_snd (eval t env)
 
 let rec read_back_nf size nf =
   match nf with
+  (* Functions *)
   | D.Normal (D.Pi (src, dest), f) ->
     let arg = mk_var src size in
     let nf = D.Normal (do_clos dest arg, do_ap f arg) in
     Syn.Lam (read_back_nf (size + 1) nf)
+  (* Numbers *)
   | D.Normal (D.Nat, D.Zero) -> Syn.Zero
   | D.Normal (D.Nat, D.Suc nf) -> Syn.Suc (read_back_nf size (D.Normal (D.Nat, nf)))
   | D.Normal (D.Nat, D.Neutral (_, ne)) -> read_back_ne size ne
+  (* Pairs *)
+  | D.Normal (D.Sig (fst, snd), p) ->
+    let fst' = do_fst p in
+    let snd = do_clos snd fst' in
+    let snd' = do_snd p in
+    Syn.Pair
+      (read_back_nf size (D.Normal (fst, fst')),
+       read_back_nf size (D.Normal (snd, snd')))
+  (* Types *)
   | D.Normal (D.Uni _, D.Nat) -> Syn.Nat
   | D.Normal (D.Uni i, D.Pi (src, dest)) ->
     let var = mk_var src size in
@@ -116,6 +151,9 @@ and read_back_tp size d =
   | D.Pi (src, dest) ->
     let var = mk_var src size in
     Syn.Pi (read_back_tp size src, read_back_tp (size + 1) (do_clos dest var))
+  | D.Sig (fst, snd) ->
+    let var = mk_var fst size in
+    Syn.Sig (read_back_tp size fst, read_back_tp (size + 1) (do_clos snd var))
   | D.Uni k -> Syn.Uni k
   | _ -> failwith "Not a type in read_back_tp"
 
@@ -133,6 +171,8 @@ and read_back_ne size ne =
     let applied_suc = do_clos2 suc tp_var suc_var in
     let suc' = read_back_nf (size + 2) (D.Normal (applied_suc_tp, applied_suc)) in
     Syn.NRec (tp', read_back_nf size zero, suc', read_back_ne size n)
+  | D.Fst ne -> Syn.Fst (read_back_ne size ne)
+  | D.Snd ne -> Syn.Snd (read_back_ne size ne)
 
 let rec initial_env env =
   match env with
