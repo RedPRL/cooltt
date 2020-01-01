03f4ac05c3a231dca4fad45604a708801b6a933f
Jonathan Sterling <jon@jonmsterling.com>
2019-12-31 20:39:59 -0500

    abstract driver environment

--- src/lib/driver.ml
+++ src/lib/driver.ml
@@ -2,15 +2,42 @@ module CS = Concrete_syntax
 module S = Syntax
 module D = Domain
 
-type env = Env of {check_env : Check.env; bindings : string list}
-
-let initial_env = Env {check_env = Check.Env.empty ; bindings = []}
-
 type error = Unbound_variable of CS.ident
 exception ElabError of error
 
+module Env : sig
+  type t
+  val init : t
+  val bindings : t -> CS.ident list
+  val check_env : t -> Check.Env.t
+  val push_name : CS.ident -> t -> t
+  val push_names : CS.ident list -> t -> t
+  val find_idx : CS.ident -> t -> int
+  val add_entry : Check.Env.entry -> t -> t
+end = 
+struct
+  type t = {check_env : Check.env; bindings : string list}
+  let init = {check_env = Check.Env.empty; bindings = []}
+  let bindings {bindings; _} = bindings
+  let check_env {check_env; _} = check_env
+  let push_name i env = {env with bindings = i :: env.bindings }
+  let push_names is env = {env with bindings = is @ env.bindings }
+
+  let find_idx key env =
+    let rec go i = 
+      function
+      | [] -> raise @@ ElabError (Unbound_variable key)
+      | x :: xs -> if String.equal x key then i else go (i + 1) xs 
+    in
+    go 0 @@ bindings env
+
+  let add_entry e env = 
+    {env with check_env = Check.Env.add_entry e env.check_env}
+end
+
+
 type output =
-    NoOutput of env
+    NoOutput of Env.t
   | NF_term of S.t * S.t
   | NF_def of CS.ident * S.t
   | Quit
@@ -35,13 +62,6 @@ let output =
     Format.fprintf Format.std_formatter "@]@,"
   | Quit -> exit 0
 
-let find_idx key =
-  let rec go i = 
-    function
-    | [] -> raise @@ ElabError (Unbound_variable key)
-    | x :: xs -> if String.equal x key then i else go (i + 1) xs in
-  go 0
-
 let rec int_to_term = 
   function
   | 0 -> S.Zero
@@ -52,11 +72,11 @@ let rec unravel_spine f =
   | [] -> f
   | x :: xs -> unravel_spine (x f) xs
 
-let rec bind env = 
+let rec bind (env : Env.t) = 
   function
-  | CS.Var i -> S.Var (find_idx i env)
+  | CS.Var i -> S.Var (Env.find_idx i env)
   | CS.Let (tp, B {name; body}) ->
-    S.Let (bind env tp, bind (name :: env) body)
+    S.Let (bind env tp, bind (Env.push_name name env) body)
   | CS.Check {term; tp} -> S.Check (bind env term, bind_ty env tp)
   | CS.Suc t -> S.Suc (bind env t)
   | CS.Lit i -> int_to_term i
@@ -66,15 +86,15 @@ let rec bind env =
         suc = B2 {name1 = suc_name1; name2 = suc_name2; body = suc_body};
         nat} ->
     S.NRec
-      (bind_ty (mot_name :: env) mot_body,
+      (bind_ty (Env.push_name mot_name env) mot_body,
        bind env zero,
-       bind (suc_name2 :: suc_name1 :: env) suc_body,
+       bind (Env.push_names [suc_name2;suc_name1] env) suc_body,
        bind env nat)
   | CS.Lam (BN {names = []; body}) ->
     bind env body
   | CS.Lam (BN {names = x :: names; body}) ->
     let lam = CS.Lam (BN {names; body}) in
-    S.Lam (bind (x :: env) lam)
+    S.Lam (bind (Env.push_name x env) lam)
   | CS.Ap (f, args) ->
     List.map (bind_spine env) args |> unravel_spine (bind env f)
   | CS.Pair (l, r) -> S.Pair (bind env l, bind env r)
@@ -85,10 +105,11 @@ let rec bind env =
        refl = B {name = refl_name; body = refl_body};
        eq} ->
     S.J
-      (bind_ty (prf :: right :: left :: env) mot_body,
-       bind (refl_name :: env) refl_body,
+      (bind_ty (Env.push_names [prf; right; left] env) mot_body,
+       bind (Env.push_name refl_name env) refl_body,
        bind env eq)
-  | CS.Refl t -> S.Refl (bind env t)
+  | CS.Refl t -> 
+    S.Refl (bind env t)
   | _ -> failwith "driver expected term"
 
 and bind_ty env =
@@ -96,11 +117,11 @@ and bind_ty env =
   | CS.Sg ([], body) ->
     bind_ty env body
   | CS.Sg (Cell cell :: tele, body) ->
-    S.Sg (bind_ty env cell.tp, bind_ty (cell.name :: env) (CS.Sg (tele, body)))
+    S.Sg (bind_ty env cell.tp, bind_ty (Env.push_name cell.name env) (CS.Sg (tele, body)))
   | CS.Pi ([], body) ->
     bind_ty env body
   | CS.Pi (Cell cell :: tele, body) ->
-    S.Pi (bind_ty env cell.tp, bind_ty (cell.name :: env) (CS.Pi (tele, body)))
+    S.Pi (bind_ty env cell.tp, bind_ty (Env.push_name cell.name env) (CS.Pi (tele, body)))
   | CS.Nat -> S.Nat
   | CS.Id (tp, left, right) ->
     S.Id (bind_ty env tp, bind env left, bind env right)
@@ -113,31 +134,33 @@ and bind_spine env =
   function
   | CS.Term t -> fun f -> S.Ap (f, bind env t)
 
-let process_decl (Env {check_env; bindings}) = 
+let process_decl env = 
   function
   | CS.Def {name; def; tp} ->
-    let def = bind bindings def in
-    let tp = bind_ty bindings tp in
+    let def = bind env def in
+    let tp = bind_ty env tp in
+    let check_env = Env.check_env env in
     Check.check_tp ~env:check_env ~tp;
     let sem_env = Check.Env.to_sem_env check_env in
     let sem_tp = Nbe.eval_tp tp sem_env in
     Check.check ~env:check_env ~term:def ~tp:sem_tp;
     let sem_def = Nbe.eval def sem_env in
     let new_entry = Check.Env.TopLevel {term = sem_def; tp = sem_tp} in
-    NoOutput (Env {check_env = Check.Env.add_entry new_entry check_env; bindings = name :: bindings })
+    NoOutput (Env.add_entry new_entry @@ Env.push_name name env)
   | CS.NormalizeDef name ->
     let err = ElabError (Unbound_variable name) in 
     begin
-      match Check.Env.get_entry check_env (find_idx name bindings) with
+      match Check.Env.get_entry (Env.check_env env) (Env.find_idx name env) with
       | Check.Env.TopLevel {term; tp} -> NF_def (name, Nbe.read_back_nf 0 (D.Nf {term; tp}))
       | _ -> raise err
       | exception Failure _ -> raise err
     end
   | CS.NormalizeTerm {term; tp} ->
-    let term = bind bindings term in
-    let tp = bind_ty bindings tp in
+    let term = bind env term in
+    let tp = bind_ty env tp in
+    let check_env = Env.check_env env in
     Check.check_tp ~env:check_env ~tp;
-    let sem_env = Check.Env.to_sem_env check_env in
+    let sem_env = Check.Env.to_sem_env check_env in 
     let sem_tp = Nbe.eval_tp tp sem_env in
     Check.check ~env:check_env ~term ~tp:sem_tp;
     let sem_term = Nbe.eval term sem_env in
@@ -145,15 +168,13 @@ let process_decl (Env {check_env; bindings}) =
     NF_term (term, norm_term)
   | CS.Quit -> Quit
 
-let rec process_sign ?env = 
+let rec process_sign_loop env = 
   function
   | [] -> ()
   | d :: ds ->
-    let env = match env with
-        None -> initial_env
-      | Some e -> e in
     let o = process_decl env d in
     output o;
-    process_sign ?env:(Some (update_env env o)) ds
+    process_sign_loop (update_env env o) ds
 
-let process_sign : Concrete_syntax.signature -> unit = process_sign
+let process_sign : Concrete_syntax.signature -> unit = 
+  process_sign_loop Env.init 
