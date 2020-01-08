38399a475af7e72cfc63de2790999d0b00c91528
Jon Sterling <jon@jonmsterling.com>
2020-01-08 17:11:15 -0500

    stubbed unification (performs no unification)

--- src/lib/Unify.ml
+++ src/lib/Unify.ml
@@ -2,10 +2,18 @@ open CoolBasis
 
 module D = Domain
 module EM = ElabBasics
+module St = ElabState
 
 open Monad.Notation (EM)
 
 exception UnificationError
+
+let eta_contract : D.con -> int EM.m =
+  function
+  | D.Cut {cut = (D.Var lvl, []), _; _} ->
+    EM.ret lvl
+  | _ -> 
+    EM.throw UnificationError
 
 let rec unify_tp : D.tp -> D.tp -> unit EM.m =
   fun tp0 tp1 ->
@@ -30,7 +38,54 @@ let rec unify_tp : D.tp -> D.tp -> unit EM.m =
     EM.ret ()
   | D.Univ, D.Univ -> 
     EM.ret ()
-  | D.El cut0, D.El cut1 ->
-    EM.lift_qu @@ Nbe.equate_cut cut0 cut1 (* TODO: check for flex *)
+  | D.El (D.Global sym0, spine0), _ ->
+    unify_tp_cut_with_tp (sym0, spine0) tp1
+  | _, D.El (D.Global sym1, spine1) ->
+    unify_tp_cut_with_tp (sym1, spine1) tp0
   | _ ->
     EM.throw UnificationError
+
+
+and unify_tp_cut_with_tp (sym0, spine0) tp1 = 
+  let* st = EM.get in
+  match St.get_flexity sym0 st with
+  | `Flex ->
+    begin
+      match tp1 with 
+      | D.El (D.Global sym1, spine1) when St.get_flexity sym1 st = `Flex -> 
+        unify_tp_flex_flex (sym0, spine0) (sym1, spine1)
+      | _ -> 
+        unify_tp_flex_rigid (sym0, spine0) tp1
+    end
+  | `Rigid ->
+    begin
+      match tp1 with
+      | D.El (D.Global sym1, spine1) when St.get_flexity sym1 st = `Flex -> 
+        unify_tp_flex_rigid (sym1, spine1) tp1
+      | D.El (D.Global sym1, spine1) when St.get_flexity sym1 st = `Rigid ->
+        unify_tp_rigid_rigid (sym0, spine0) (sym1, spine1)
+      | _ -> 
+        EM.throw UnificationError
+    end
+
+and unify_tp_flex_rigid (sym0, spine0) tp1 =
+  (* TODO *)
+  EM.lift_qu @@ 
+  Nbe.equate_tp
+    (D.El (D.Global sym0, spine0) )
+    tp1
+
+
+and unify_tp_flex_flex (sym0, spine0) (sym1, spine1) =
+  (* TODO *)
+  EM.lift_qu @@ 
+  Nbe.equate_cut 
+    (D.Global sym0, spine0) 
+    (D.Global sym1, spine1)
+
+and unify_tp_rigid_rigid (sym0, spine0) (sym1, spine1) =
+  (* TODO *)
+  EM.lift_qu @@ 
+  Nbe.equate_cut 
+    (D.Global sym0, spine0) 
+    (D.Global sym1, spine1)
