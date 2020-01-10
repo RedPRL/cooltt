02cd5b0e6f22e9aa9b66caea44562dcaaae78a87
Jonathan Sterling <jon@jonmsterling.com>
2020-01-10 11:21:13 -0500

    foo

--- src/lib/Domain.ml
+++ src/lib/Domain.ml
@@ -51,10 +51,14 @@ and frm =
   | KGoalProj
 
 
-let pp_tp fmt _ = Format.fprintf fmt "<tp>"
-let pp_con fmt _ = Format.fprintf fmt "<con>"
+let pp_tp fmt _ = 
+  Format.fprintf fmt "<tp>"
+
+let pp_con fmt _ = 
+  Format.fprintf fmt "<con>"
 
 let push frm (hd, sp) = 
   hd, sp @ [frm]
 
-let mk_var tp lev = Cut {tp; cut = (Var lev, []), None}
+let mk_var tp lev = 
+  Cut {tp; cut = (Var lev, []), None}
