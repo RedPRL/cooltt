027158a83e4731166f30df73f0e377b0ebcded82
Jonathan Sterling <jon@jonmsterling.com>
2020-03-09 11:01:14 -0400

    get rid of some annoying duplication

--- src/lib/Syntax.ml
+++ src/lib/Syntax.ml
@@ -1,53 +1,5 @@
 open CoolBasis open Bwd
-
-type dim =
-  | Dim0
-  | Dim1
-  | DimVar of int (* De Bruijn index *)
-
-type cof = (int, dim) Cof.cof
-
-type t =
-  | Var of int (* DeBruijn indices for variables *)
-  | Global of Symbol.t
-  | Let of t * t
-  | Ann of t * tp
-  | Zero
-  | Suc of t
-  | NatElim of ghost option * tp * t * t * t
-  | Lam of t
-  | Ap of t * t
-  | DimLam of t
-  | DimAp of t * dim
-  | Pair of t * t
-  | Fst of t
-  | Snd of t
-  | Refl of t
-  | IdElim of ghost option * tp * t * t
-  | GoalRet of t
-  | GoalProj of t
-  | Coe of t * dim * dim * t
-  | HCom of t * dim * dim * cof * t
-  | TpCode of t gtp
-  | CofTree of cof_tree
-
-and cof_tree = (int, dim, t) Cof.tree
-
-and tp = Tp of tp gtp
-
-and _ gtp =
-  | Nat : 'a gtp
-  | Pi : 'a * 'a -> 'a gtp
-  | Sg : 'a * 'a -> 'a gtp
-  | Id : 'a * t * t -> 'a gtp
-  | DimPi : 'a -> 'a gtp
-  | Sub : 'a * cof * t -> 'a gtp
-  | Univ : tp gtp
-  | El : t -> tp gtp
-  | GoalTp : string option * tp -> tp gtp
-
-
-and ghost = string bwd * [`Con of (tp * t) | `Dim of dim | `Cof of cof] list
+include SyntaxData
 
 let rec condense = 
   function
--- src/lib/Syntax.mli
+++ src/lib/Syntax.mli
@@ -1,57 +1,7 @@
-open CoolBasis.Bwd
-
-type dim =
-  | Dim0
-  | Dim1
-  | DimVar of int
-
-type cof = (int, dim) Cof.cof
-
-type t =
-  | Var of int
-  | Global of Symbol.t
-  | Let of t * t
-  | Ann of t * tp
-  | Zero
-  | Suc of t
-  | NatElim of ghost option * tp * t * t * t
-  | Lam of t
-  | Ap of t * t
-  | DimLam of t
-  | DimAp of t * dim
-  | Pair of t * t
-  | Fst of t
-  | Snd of t
-  | Refl of t
-  | IdElim of ghost option * tp * t * t
-  | GoalRet of t
-  | GoalProj of t
-  | Coe of t * dim * dim * t
-  | HCom of t * dim * dim * cof * t
-  | TpCode of t gtp
-  | CofTree of cof_tree
-
-and cof_tree = (int, dim, t) Cof.tree
-
-and tp = Tp of tp gtp
-
-and _ gtp =
-  | Nat : 'a gtp
-  | Pi : 'a * 'a -> 'a gtp
-  | Sg : 'a * 'a -> 'a gtp
-  | Id : 'a * t * t -> 'a gtp
-  | DimPi : 'a -> 'a gtp
-  | Sub : 'a * cof * t -> 'a gtp
-  | Univ : tp gtp
-  | El : t -> tp gtp
-  | GoalTp : string option * tp -> tp gtp
-
-and ghost = string bwd * [`Con of (tp * t) | `Dim of dim | `Cof of cof] list
-
-type env = tp list
+include module type of SyntaxData
 
 open CoolBasis
+
 val pp : Pp.env -> t Pp.printer
 val pp_tp : Pp.env -> tp Pp.printer
-
 val pp_sequent : names:string list -> tp Pp.printer
