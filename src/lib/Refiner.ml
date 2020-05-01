49313263b8ddfe77f49c0226bd47041d5aab1ec6
Jonathan Sterling <jon@jonmsterling.com>
2020-05-01 11:28:57 -0400

    begin adding a sick "auto-hcom" tactic

--- src/lib/Refiner.ml
+++ src/lib/Refiner.ml
@@ -519,28 +519,69 @@ struct
     and+ fam_trg = EM.lift_ev @@ Nbe.eval_tp @@ S.El (S.Ap (fam, trg)) in
     S.Coe (fam, src, trg, tm), fam_trg
 
-  let hcom tac_ty tac_src tac_trg tac_cof tac_tm : T.syn_tac =
-    let* ty = tac_ty D.Univ in
+
+  let hcom_bdy_tp tp r phi =
+    EM.lift_cmp @@
+    Nbe.quasiquote_tp @@
+    QQ.foreign r @@ fun src ->
+    QQ.foreign (D.cof_to_con phi) @@ fun cof ->
+    QQ.foreign_tp tp @@ fun vtp ->
+    QQ.term @@
+    TB.pi TB.tp_dim @@ fun i ->
+    TB.pi (TB.tp_prf (TB.join (TB.eq i src) cof)) @@ fun _ ->
+    vtp
+
+  let hcom tac_code tac_src tac_trg tac_cof tac_tm : T.syn_tac =
+    let* code = tac_code D.Univ in
     let* src = tac_src D.TpDim in
     let* trg = tac_trg D.TpDim in
     let* cof = tac_cof D.TpCof in
     let* vsrc = EM.lift_ev @@ Nbe.eval src in
     let* vcof = EM.lift_ev @@ Nbe.eval_cof cof in
-    let* velty = EM.lift_ev @@ Nbe.eval_tp @@ S.El ty in
+    let* vtp = EM.lift_ev @@ Nbe.eval_tp @@ S.El code in
     (* (i : dim) -> (_ : [i=src \/ cof]) -> A *)
-    let+ tm =
+    let+ tm = tac_tm @<< hcom_bdy_tp vtp vsrc vcof in
+    S.HCom (code, src, trg, cof, tm), vtp
+
+  let auto_hcom tac_code tac_src tac_trg tac_tm : T.bchk_tac =
+    fun (vtp, vpsi, clo) ->
+    let* code = tac_code D.Univ in
+    let* elcode = EM.lift_ev @@ Nbe.eval_tp @@ S.El code in
+    let* () = EM.equate_tp vtp elcode in
+    let* psi = EM.lift_qu @@ Nbe.quote_cof vpsi in
+    let* src = tac_src D.TpDim in
+    let* trg = tac_trg D.TpDim in
+    let* vsrc = EM.lift_ev @@ Nbe.eval src in
+    let* vtrg = EM.lift_ev @@ Nbe.eval trg in
+    let* tm =
       tac_tm @<<
       EM.lift_cmp @@
       Nbe.quasiquote_tp @@
       QQ.foreign vsrc @@ fun src ->
-      QQ.foreign (D.cof_to_con vcof) @@ fun cof ->
-      QQ.foreign_tp velty @@ fun velty ->
+      QQ.foreign vtrg @@ fun trg ->
+      QQ.foreign (D.Lam clo) @@ fun pel ->
+      QQ.foreign (D.cof_to_con vpsi) @@ fun cof ->
+      QQ.foreign_tp vtp @@ fun tp ->
       QQ.term @@
       TB.pi TB.tp_dim @@ fun i ->
       TB.pi (TB.tp_prf (TB.join (TB.eq i src) cof)) @@ fun _ ->
-      velty
+      TB.sub tp (TB.meet (TB.eq i trg) cof) @@ fun prf -> TB.ap pel [prf]
     in
-    S.HCom (ty, src, trg, cof, tm), velty
+    let* vtm = EM.lift_ev @@ Nbe.eval tm in
+    let* vtm' =
+      EM.lift_cmp @@ Nbe.quasiquote_tm @@
+      QQ.foreign vtm @@ fun tm ->
+      QQ.term @@
+      TB.lam @@ fun i ->
+      TB.lam @@ fun prf ->
+      TB.sub_out @@
+      TB.ap tm [i; prf]
+    in
+    let* tm' =
+      let* bdy_tp  = hcom_bdy_tp vtp vsrc vpsi in
+      EM.lift_qu @@ Nbe.quote_con bdy_tp vtm'
+    in
+    EM.ret @@ S.HCom (code, src, trg, psi, tm')
 
   let com tac_fam tac_src tac_trg tac_cof tac_tm : T.syn_tac =
     let* piuniv =
