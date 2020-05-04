885eb99e77239d7cd675332eaae65dd84e4f65c7
Ian Ernest Voysey <iev@cs.cmu.edu>
2020-05-04 12:51:26 -0400

    whitespace, also using quote_dim instead of its definition

--- src/lib/Nbe.ml
+++ src/lib/Nbe.ml
@@ -1018,20 +1018,20 @@ struct
       let* tm1 = branch_body phi1 clo1 in
       ret @@ S.CofSplit (ttp, tphi0, tphi1, tm0, tm1)
 
-  (* stolen from quote_cof;we should replace those and other uses with calls to this, if this is the right thing *) 
-  and quote_dim d = quote_con D.TpDim @@ D.dim_to_con d 
+  (* stolen from quote_cof;we should replace those and other uses with calls to this, if this is the right thing *)
+  and quote_dim d = quote_con D.TpDim @@ D.dim_to_con d
     (* this logis is right but ends up not being called because which variables are dimensions is lost;; todo/iev *)
     (* function
     | D.Dim0 -> ret S.Dim0
     | D.Dim1 -> ret S.Dim1
-    | D.DimVar lvl -> 
-      let* eq0 = lift_cmp @@ CmpM.test_sequent [] (Cof.eq (D.DimVar lvl) D.Dim0) in 
-      let* eq1 = lift_cmp @@ CmpM.test_sequent [] (Cof.eq (D.DimVar lvl) D.Dim1) in 
+    | D.DimVar lvl ->
+      let* eq0 = lift_cmp @@ CmpM.test_sequent [] (Cof.eq (D.DimVar lvl) D.Dim0) in
+      let* eq1 = lift_cmp @@ CmpM.test_sequent [] (Cof.eq (D.DimVar lvl) D.Dim1) in
       (* check to if the ctx can prove that the variable in question is either 0
       or 1 before quoting just the level *)
       if eq0 then ret S.Dim0
       else if eq1 then ret S.Dim1
-      else 
+      else
         let+ ix = quote_var lvl in
         S.Var ix
     | D.DimProbe _ ->
@@ -1045,8 +1045,8 @@ struct
     | Cof.Cof phi ->
       match phi with
       | Cof.Eq (r, s) ->
-        let+ tr = quote_con D.TpDim @@ D.dim_to_con r
-        and+ ts = quote_con D.TpDim @@ D.dim_to_con s in
+        let+ tr = quote_dim r
+        and+ ts = quote_dim s in
         S.Cof (Cof.Eq (tr, ts))
 
       | Cof.Join (phi, psi) ->
