e6ada209afb48f9b1218dcac8a6a47664dabb3dc
favonia <favonia@gmail.com>
2020-05-11 23:59:13 -0500

    Where did these space characters come from?

--- src/lib/CofEnv.mli
+++ src/lib/CofEnv.mli
@@ -24,6 +24,6 @@ val test_sequent : env -> D.cof list -> D.cof -> bool
 (** Monadic interface *)
 module M (M : CoolBasis.Monad.S) :
 sig
-    (* Search all branches induced by unreduced joins under additional cofibrations. *)
-    val left_invert_under_cofs : env -> D.cof list -> (env -> unit M.m) -> unit M.m
+  (* Search all branches induced by unreduced joins under additional cofibrations. *)
+  val left_invert_under_cofs : env -> D.cof list -> (env -> unit M.m) -> unit M.m
 end
