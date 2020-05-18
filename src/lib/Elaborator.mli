c8ffb89ab6f1761c1f3e93bdf97f1ac965d28276
favonia <favonia@gmail.com>
2020-05-17 23:15:06 -0500

    Revert whitespace changes in this PR.

--- src/lib/Elaborator.mli
+++ src/lib/Elaborator.mli
@@ -10,5 +10,4 @@ val chk_tp_in_tele : CS.cell list -> CS.con -> tp_tac
 val chk_tm : CS.con -> chk_tac
 val chk_tm_in_tele : CS.cell list -> CS.con -> chk_tac
 val bchk_tm : CS.con -> bchk_tac
-
 val syn_tm : CS.con -> syn_tac
