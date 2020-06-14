edbad258c517da2bcff2abf0b481a6582c2f5643
Jonathan Sterling <jon@jonmsterling.com>
2020-06-13 21:12:53 -0400

    document Dim

--- src/cubical/Dim.mli
+++ src/cubical/Dim.mli
@@ -2,7 +2,14 @@ open Basis
 
 type dim =
   | Dim0
+    (** The left endpoint of the abstract interval. *)
+
   | Dim1
+    (** The right endpoint of the abstract interval. *)
+
   | DimVar of int
+    (** In [cooltt], most dimension variables are represented as natural numbers (pointers into the context). *)
+
   | DimSym of Symbol.t
+    (** Some dimension variables must be generated in a globally fresh way ({i e.g.} when computing under a binder). *)
 
