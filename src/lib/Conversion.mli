bf47b0e331277db6f2de44eb71779dfcd099ab2a
Jonathan Sterling <jon@jonmsterling.com>
2020-06-13 22:54:56 -0400

    documenting and moving Low Ch'i code

--- src/lib/Conversion.mli
+++ src/lib/Conversion.mli
@@ -1,13 +1,16 @@
+(** The purpose of this module is to check whether two {i well-typed} objects are equal or not. The semantics are that all definitions are unfolded, regardless of the "veil", since definitional equality is closed under unfolding of definitions. *)
+
 module D := Domain
-open Basis 
+open Basis
 open Monads
 
-module Error : 
+module Error :
 sig
   type t
   val pp : t Pp.printer
 end
 
+exception ConversionError of Error.t
+
 val equate_con : D.tp -> D.con -> D.con -> unit conversion
 val equate_tp : D.tp -> D.tp -> unit conversion
-val trap_err : unit ElabM.m -> [`Ok | `Err of Error.t] ElabM.m
