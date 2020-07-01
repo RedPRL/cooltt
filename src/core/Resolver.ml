583b89ce8b12e7c72712e3d48d5acb7275e61e12
Jonathan Sterling <jon@jonmsterling.com>
2020-07-01 14:20:23 -0400

    types

--- src/core/Resolver.ml
+++ src/core/Resolver.ml
@@ -1,9 +1,10 @@
 open Basis
 module SymMap = Map.Make (Symbol)
-module IntMap = Map.Make (Int)
+module NativeMap = Map.Make (Int)
 module StringMap = Map.Make (String)
 
 type symbol = Symbol.t
+type native = int
 
 module Env :
 sig
@@ -17,18 +18,18 @@ sig
 end =
 struct
   type env =
-    {info_of_string : [`Native of int] StringMap.t;
-     string_of_native : string IntMap.t;
-     info_of_native : symbol IntMap.t;
-     native_of_sym : int SymMap.t}
+    {info_of_string : [`Native of native] StringMap.t;
+     string_of_native : string NativeMap.t;
+     info_of_native : symbol NativeMap.t;
+     native_of_sym : native SymMap.t}
 
   let empty =
     {info_of_string = StringMap.empty;
-     string_of_native = IntMap.empty;
-     info_of_native = IntMap.empty;
+     string_of_native = NativeMap.empty;
+     info_of_native = NativeMap.empty;
      native_of_sym = SymMap.empty}
 
-  let native_of_sym sym env : int option =
+  let native_of_sym sym env : native option =
     SymMap.find_opt sym env.native_of_sym
 
   let add_native (str : string option) (sym : symbol) (env : env) : env =
@@ -37,9 +38,9 @@ struct
       | Some native ->
         native, env.info_of_native, env.native_of_sym
       | None ->
-        let native = IntMap.cardinal env.info_of_native in
+        let native = NativeMap.cardinal env.info_of_native in
         native,
-        IntMap.add native sym env.info_of_native,
+        NativeMap.add native sym env.info_of_native,
         SymMap.add sym native env.native_of_sym
     in
 
@@ -50,12 +51,12 @@ struct
         match StringMap.find_opt str env.info_of_string with
         | None ->
           StringMap.add str (`Native native) env.info_of_string,
-          IntMap.add native str env.string_of_native
+          NativeMap.add native str env.string_of_native
         | Some (`Native old_native) when old_native = native ->
           env.info_of_string, env.string_of_native
         | Some (`Native old_native) ->
           StringMap.add str (`Native native) env.info_of_string,
-          IntMap.add native str @@ IntMap.remove old_native env.string_of_native
+          NativeMap.add native str @@ NativeMap.remove old_native env.string_of_native
     in
 
     {info_of_native; native_of_sym; info_of_string; string_of_native}
@@ -63,13 +64,13 @@ struct
   let resolve str env =
     match StringMap.find_opt str env.info_of_string with
     | Some (`Native native) ->
-      IntMap.find_opt native env.info_of_native
+      NativeMap.find_opt native env.info_of_native
     | None -> None
 
   let unresolve sym env =
     match SymMap.find_opt sym env.native_of_sym with
     | Some native ->
-      IntMap.find_opt native env.string_of_native
+      NativeMap.find_opt native env.string_of_native
     | None -> None
 
 end
--- src/core/Resolver.mli
+++ src/core/Resolver.mli
@@ -1,5 +1,4 @@
 open Basis
-module Y := Yuujinchou
 
 type env
 type symbol = Symbol.t
@@ -7,5 +6,8 @@ type symbol = Symbol.t
 val empty : env
 val resolve : string -> env -> symbol option
 val unresolve : symbol -> env -> string option
+
+(** A [native] is an address to a declaration in the {i current} unit. *)
+type native = int
 
 val add_native : string option -> symbol -> env -> env
