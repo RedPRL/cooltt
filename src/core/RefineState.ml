531dba9a0355352374693b3a8ee67f67e4dc1c47
Reed Mullanix <reedmullanix@gmail.com>
2021-06-02 08:30:36 -0700

    Start laying the groundwork for namespaces/imports

--- src/core/RefineState.ml
+++ src/core/RefineState.ml
@@ -3,12 +3,36 @@ open Basis
 module D = Domain
 module StringMap = Map.Make (String)
 
+type namespace = {
+    (** Used to resolve an identifier to a unique symbol in this namespace. *)
+    symbols : Symbol.t StringMap.t;
+    (** Namespaces may themselves contain other namespaces, so let's keep track of them. *)
+    namespaces : namespace StringMap.t
+  }
+
+(* FIXME Implemnt adding of qualified names *)
+let add_symbol (ident : Ident.t) sym ns =
+  match ident with
+  | `User ident -> { ns with symbols = StringMap.add ident sym ns.symbols }
+  | _           -> ns
+
+(* FIXME Implemnt resolution of qualified names *)
+let resolve_symbol (ident : Ident.t) ns =
+  match ident with
+  | `User ident -> StringMap.find_opt ident ns.symbols
+  | _           -> None
+
+
 type t =
-  {resolver : Symbol.t StringMap.t;
-   globals : (D.tp * D.con option) SymbolMap.t}
+  { (** A nested series of namespaces. The top-level namespace corresponds to the current one. *)
+    resolver : namespace;
+    (** The set of bindings currently in scope. This includes all bindings from the namespaces *)
+    (* FIXME: Perhaps we ought to use an array here? We could get O(1) indexing. *)
+    globals : (D.tp * D.con option) SymbolMap.t
+  }
 
 let init =
-  {resolver = StringMap.empty;
+  {resolver = { symbols = StringMap.empty; namespaces = StringMap.empty };
    globals = SymbolMap.empty}
 
 let add_global (ident : Ident.t) tp ocon st =
@@ -20,12 +44,7 @@ let add_global (ident : Ident.t) tp ocon st =
     | `Anon -> None
   in
   sym,
-  {resolver =
-     begin
-       match ident with
-       | `User ident -> StringMap.add ident sym st.resolver
-       | _ -> st.resolver
-     end;
+  {resolver = add_symbol ident sym st.resolver;
    globals = SymbolMap.add sym (tp, ocon) st.globals}
 
 let add_flex_global tp st =
@@ -34,10 +53,7 @@ let add_flex_global tp st =
   {st with
    globals = SymbolMap.add sym (tp, None) st.globals}
 
-let resolve_global ident st =
-  match ident with
-  | `User id -> StringMap.find_opt id st.resolver
-  | _ -> None
+let resolve_global ident st = resolve_symbol ident st.resolver
 
 let get_global sym st =
   SymbolMap.find sym st.globals
