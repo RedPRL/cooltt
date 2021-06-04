5b4c49a416f52aa0734edb0484660a7161a49e3d
Jonathan Sterling <jonsterling@users.noreply.github.com>
2021-06-04 13:07:54 -0400

    Dimension probe invariants (#191)
    * add special DimSym constructor rather than using "Global"
    
    * add DimGlobal
    
    * rename DimSym => DimProbe

--- src/core/Semantics.ml
+++ src/core/Semantics.ml
@@ -111,7 +111,7 @@ struct
     loop
 
   let forall sym =
-    let i = Dim.DimSym sym in
+    let i = Dim.DimProbe sym in
     extend @@
     function
     | `CofEq (r, s) ->
@@ -160,8 +160,9 @@ and con_to_dim =
     function
     | D.Dim0 -> ret Dim.Dim0
     | D.Dim1 -> ret Dim.Dim1
+    | D.DimProbe x -> ret @@ Dim.DimProbe x
     | D.Cut {cut = Var l, []; _} -> ret @@ Dim.DimVar l
-    | D.Cut {cut = Global sym, []; _} -> ret @@ Dim.DimSym sym
+    | D.Cut {cut = Global x, []; _} -> ret @@ Dim.DimGlobal x
     | con ->
       Format.eprintf "bad: %a@." D.pp_con con;
       throw @@ NbeFailed "con_to_dim"
@@ -189,7 +190,7 @@ and push_subst_con : D.dim -> Symbol.t -> D.con -> D.con CM.m =
     D.Lam (ident, clo)
   | BindSym (y, con) ->
     begin
-      test_sequent [] (Cof.eq (Dim.DimSym x) (Dim.DimSym y)) |>>
+      test_sequent [] (Cof.eq (Dim.DimProbe x) (Dim.DimProbe y)) |>>
       function
       | true ->
         ret @@ D.BindSym (y, con)
@@ -251,9 +252,9 @@ and push_subst_con : D.dim -> Symbol.t -> D.con -> D.con CM.m =
     and+ pivot = subst_con r x pivot
     and+ base = subst_con r x base in
     D.VIn (s, equiv, pivot, base)
-  | D.Cut {tp = _; cut = (D.Global y, [])} as con ->
+  | D.DimProbe y as con ->
     begin
-      test_sequent [] (Cof.eq (Dim.DimSym x) (Dim.DimSym y)) |>>
+      test_sequent [] (Cof.eq (Dim.DimProbe x) (Dim.DimProbe y)) |>>
       function
       | true ->
         ret @@ D.dim_to_con r
@@ -629,7 +630,7 @@ and eval : S.t -> D.con EvM.m =
       end
     | S.ForallCof tm ->
       let sym = Symbol.named "forall_probe" in
-      let i = Dim.DimSym sym in
+      let i = Dim.DimProbe sym in
       let* phi = append [D.dim_to_con i] @@ eval_cof tm in
       D.cof_to_con <@> lift_cmp @@ FaceLattice.forall sym phi
     | S.CofSplit (branches) ->
@@ -763,7 +764,7 @@ and whnf_con ~style : D.con -> D.con whnf CM.m =
   let open CM in
   function
   | D.Lam _ | D.BindSym _ | D.Zero | D.Suc _ | D.Base | D.Pair _ | D.SubIn _ | D.ElIn _ | D.LockedPrfIn _
-  | D.Cof _ | D.Dim0 | D.Dim1 | D.Prf | D.StableCode _ ->
+  | D.Cof _ | D.Dim0 | D.Dim1 | D.Prf | D.StableCode _ | D.DimProbe _ ->
     ret `Done
   | D.LetSym (r, x, con) ->
     reduce_to ~style @<< push_subst_con r x con
@@ -1397,7 +1398,7 @@ and dispatch_rigid_coe ~style line =
   in
   let peek line =
     let x = Symbol.fresh () in
-    go x @<< whnf_inspect_con ~style @<< do_ap line @@ D.dim_to_con @@ Dim.DimSym x |>>
+    go x @<< whnf_inspect_con ~style @<< do_ap line @@ D.dim_to_con @@ Dim.DimProbe x |>>
     function
     | `Reduce _ | `Done as res -> ret res
     | `Unknown ->
