f2fe94022ac04b59edfa2f2391a5f4fc82422994
favonia <favonia@gmail.com>
2021-06-07 13:25:07 -0500

    No one has to know the actual implementation of shifting.

--- src/core/ULvl.mli
+++ src/core/ULvl.mli
@@ -1,12 +1,6 @@
 open Basis
 
-(** [[10;20;22;23;24;...]] is represented as [{init=10;steps=[10;2]}] because
-    the first element is [10] and the steps between adjacent elementns are [[10;2;1;1;...]].
-    The infinite suffix [[1;1;...]] should not be part of the encoding. *)
-type shift =
-  { init: int
-  ; steps: int list (** invariants: all numbers > 0 and the last element > 1 *)
-  }
+type shift
 
 type t =
   | LvlShiftedVar of {var: int; shift: shift}
