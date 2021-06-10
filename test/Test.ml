10cde97adccb90e1c138d94d12ba9d1b3a84596f
Reed Mullanix <reedmullanix@gmail.com>
2021-06-10 11:26:09 -0700

    Sort the test file names before execution

--- test/Test.ml
+++ test/Test.ml
@@ -2,6 +2,7 @@ open Frontend
 
 let () =
   List.iter (fun fname -> ignore @@ Driver.process_file (`File fname))
+  @@ List.sort String.compare
   @@ List.filter (fun fname -> String.equal (Filename.extension fname) ".cooltt")
   @@ Array.to_list
   @@ Sys.readdir "."
