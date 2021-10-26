384209b0cbd1e015999dfc5cf9c7384da2be8428
Reed Mullanix <reedmullanix@gmail.com>
2021-10-26 11:21:17 -0700

    Add 'Ident.user', along with some subtyping constraints

--- src/core/Ident.ml
+++ src/core/Ident.ml
@@ -1,4 +1,6 @@
 type t = [`Anon | `User of string list | `Machine of string]
+type 'a some = 'a constraint 'a = [< t ]
+type user = [ `User of string list ]
 
 let user parts = `User parts
 
@@ -7,10 +9,14 @@ let qual_to_string =
   | [] -> "(root)"
   | parts -> String.concat "." parts
 
+let pp_user fmt =
+  function
+  | `User parts -> Uuseg_string.pp_utf_8 fmt (qual_to_string parts)
+
 let pp fmt =
   function
   | `Anon -> Format.fprintf fmt "<anon>"
-  | `User parts -> Uuseg_string.pp_utf_8 fmt (qual_to_string parts)
+  | `User _ as u -> pp_user fmt u
   | `Machine str -> Uuseg_string.pp_utf_8 fmt str
 
 let to_string =
@@ -24,3 +30,12 @@ let to_string_opt =
   | `User parts -> Some (qual_to_string parts)
   | `Machine nm -> Some nm
   | `Anon -> None
+
+let user_to_string_opt =
+  function
+  | `User [] -> None
+  | `User parts -> Some (String.concat "." parts)
+
+let equal i0 i1 =
+  match (i0, i1) with
+  | `User p0, `User p1 -> List.equal String.equal p0 p1
