7957c525fe9c1c5a4460b09d87e9f42a8e6bb4e9
Matthew McQuaid <matthew@returntocorp.com>
2022-03-18 22:48:29 -0700

    add infer_nullary_ext tactic, update intro_fields, add tests

--- src/core/Refiner.mli
+++ src/core/Refiner.mli
@@ -65,6 +65,7 @@ module Univ : sig
   val patch : Chk.tac -> (Ident.user -> Chk.tac option) -> Chk.tac
   val total : (Ident.user * CodeUnit.Domain.con) list -> CodeUnit.Domain.con -> Chk.tac
   val ext : int -> Chk.tac -> Chk.tac -> Chk.tac -> Chk.tac
+  val infer_nullary_ext : Chk.tac
   val code_v : Chk.tac -> Chk.tac -> Chk.tac -> Chk.tac -> Chk.tac
   val coe : Chk.tac -> Chk.tac -> Chk.tac -> Chk.tac -> Syn.tac
   val hcom : Chk.tac -> Chk.tac -> Chk.tac -> Chk.tac -> Chk.tac -> Syn.tac
