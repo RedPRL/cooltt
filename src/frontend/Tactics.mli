b45f5e0b23586ebae3ee427505f351c8c60ea63f
Matthew McQuaid <matthew@returntocorp.com>
2022-04-21 20:31:52 -0400

    comment

--- src/frontend/Tactics.mli
+++ src/frontend/Tactics.mli
@@ -10,6 +10,11 @@ module R := Refiner
 
 open Tactic
 
+(** Determines whether a signature is:
+   `TotalAll : A total space created by the `total` tactic, where all fields but `fib` are patched
+   `TotalSome : A total space created by the `total` tactic, where at least one non-`fib` field is *not* patched
+   `NotTotal : Not a total space created by the `total` tactic
+*)
 val is_total : D.sign -> [`TotalAll | `TotalSome | `NotTotal] RM.m
 
 val intro_subtypes : Chk.tac -> Chk.tac
